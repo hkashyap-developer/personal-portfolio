4add1404a6c8ea2d23e2807241dc9cb34729d507
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-15 13:33:15 +0530

    stable update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Navbar from "@/components/custom/global/navbar/navbar";
 import Herobanner from "@/components/custom/herobanner/herobanner";
+import Herobannertext from "@/components/custom/herobannertext/herobannertext";
 import Alltestimonials from "@/components/custom/global/testimonial/testimonial";
 
 export default function Home() {
   return (
     <div className="">
+      <Navbar />
+      <Herobannertext />
       <Herobanner />
       <Alltestimonials />
     </div>
